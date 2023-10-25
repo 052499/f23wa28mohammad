3d2d4622eb250977fe03ae7fef0dbd7d310ed796
Mohammad Khalida Parveen <mohammadkhalidaparveen23@gmail.com>
2023-10-25 13:30:21 -0500

    Updated users

---
+++ routes/computation.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  x=0
+if(req.query.x === undefined){
+  x = Math.random(7);
+}else{
+  x = req.query.x;
+  }
+exp = math.exp(x);
+   
+
+  // Prepare the response string
+ 
+
+  res.send("expression applied to " + x + "is" + exp);
+});
+
+module.exports = router;
